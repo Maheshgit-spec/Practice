9ba94c9071c8918a216bfefcbc62942c94a7c6a6
Maheshgit-spec <gundumahesh07@gmail.com>
2025-03-31 00:32:44 +0530

    mahesh

--- Demo1.java
+++ Demo1.java
@@ -3,7 +3,7 @@ class Demo
     public static void main(String args[])
     {
         System.out.println("wel come to my world...");
-        System.out.println("this ");
+   
            System.out.println("this new one  also");
 
     }
