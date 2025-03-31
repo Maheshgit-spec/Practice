0ddaa7d21f54bd902804c507644eb301ee5d08e8
Maheshgit-spec <gundumahesh07@gmail.com>
2025-03-31 11:25:39 +0530

    new method has been deleted

--- Demo1.java
+++ Demo1.java
@@ -3,11 +3,7 @@ class Demo
     public static void main(String args[])
     {
         System.out.println("wel come to my world...");
-   
-         public static void check()
-         {
-            System.out.println("this is the check method")
-         }
+
 
     }
 }
